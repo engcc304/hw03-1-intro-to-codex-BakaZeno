467fef455663fcf9ee9d5fddafa9142066383714
BakaZeno <agarz3no@gmail.com>
2023-06-30 13:52:43 +0700

    test10

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -7,9 +7,9 @@ int main(){
   printf("This is %d + %d = %d ",a,b,a+b);
   for(int i=0;i<d-30;i++){
     if(i<30){
-      printf("Here\n");
+      printf("Here");
       if(i==8){
-        printf("This is 8.\n");
+        printf("This is 8.");
       }
     }
     printf("ok");
