7922223fd8fa8d7029056179b93fce6090196633
BakaZeno <agarz3no@gmail.com>
2023-07-04 22:00:09 +0700

    testรอบที่ร้อยล้าน

--- Homework/HW03/hw03-1-codex.cpp
+++ Homework/HW03/hw03-1-codex.cpp
@@ -7,12 +7,10 @@ int main(){
   printf("This is %d + %d = %d ",a,b,a+b);
   for(int i=0;i<d-30;i++){
     if(i<30){
-      printf("Here");
-      if(i==8){
-        printf("This is 8.");
+      printf("Here ");
+    if(i==8){
+      printf("This is 8. ");
       }
-    }
-    printf("ok");
-  }
-  return 0;
+    }printf("ok");
+  }return 0;
 }
